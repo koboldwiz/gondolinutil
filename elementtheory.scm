d1499ca52595ced62c415fa1609f3dc28dc683f8
The Holy Ghost <holyghost@ip-172-31-85-48.ec2.internal>
2020-08-12 03:58:29 +0000

    added first sampling method for elementtheory (delimiter count)

--- elementtheory.scm
+++ elementtheory.scm
@@ -2,19 +2,42 @@
 ;; FIXME also make this for streams
 
 (define (make-element-theory)
-	(let ((*delimiters '(" ", ",")) 
+	(let ((*delimiters '((string #\newline) 
+				(string #\tab) ;;FIXME
+				" " 
+				",")) 
 		)
 
+	;; calculates max in list of numbers and returns its position
+	(define (argmax l)
+		(let ((max -1)
+			(maxindex 0))
+			(do ((i 0 (+ i 1)))
+				((null? l) maxindex)
+				(if (> (car l) max)
+					(begin 
+						(set! max (car l))
+						(set! maxindex i)))
+				(set! l (cdr l))) 
+		))
+
+	;; this is the simplest sampler based on the count of the delimiters
+	;; stored in *delimiters
+	;; NOTE : a hash with cache can be used or a sampler such as
+	;;        Metropolis-Hastings algorithm
 	(define (prove-theory collection)
-		(do ((delimiters *delmiters (cdr delimiters)))
-			((null? delimiters) #t)
+		(let ((l '()))
+		(do ((delimiters *delimiters (cdr delimiters)))
+			((null? delimiters) (argmax l))
 			(let ((delimiter-count 0))
 			(do ((coll collection (cdr coll)))
-				((null? coll) #t)
-				(if (eq? ((car coll) 'get) (car delimiters)) 
+				((null? coll) (set! l 
+						(append (list delimiter-count)
+							 l)))
+				(if (eq? (((car coll) 'get)) (car delimiters)) 
 					(set! delimiter-count (+ 1 delimiter-count)))
 				)) 
-		))
+		))))
 
 	(define (dispatch msg)
 		(cond ((eq? msg 'prove) prove-theory)
